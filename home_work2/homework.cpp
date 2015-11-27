a10d449225258cdbd830b897f76b8a629284fcf6
SeongGue <game_son20@naver.com>
2015-11-28 01:08:57 +0900

    롤러코스터 움직임 구현

--- home_work2/homework.cpp
+++ home_work2/homework.cpp
@@ -13,6 +13,8 @@ void Mouse_Move(int x, int y);
 void Init();
 void Cmera_change();
 bool Collide_XZ(float a_x, float a_z, float b_x, float b_z, float a_radious, float b_radious);
+void draw_point();
+void Hermite_Spline();
 
 enum { XY_SURFACE = 0, XZ_SURFACE = 1, PERSPECTIVE = 2 };
 
@@ -26,10 +28,15 @@ struct Point
 
 Point p[POINT_NUM];
 Point test_point = { 0, 0, 0 };
+Point save_spline_point[100];
 
 int camera_viewpoint = 0;
 int w1 = 0, h1 = 0;
 int point_num = 0;
+
+int s_index = 0;
+
+float angle = 0;
 
 
 void main(int argc, char *argv[])
@@ -53,67 +60,17 @@ GLvoid drawScene(GLvoid)
 	Cmera_change();
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // 설정된 색으로 젂체를 칠하기
-	glColor3f(1, 1, 1);
-	Point v0 = { 3 * (p[1].x - p[0].x), 3 * (p[1].y - p[0].y), 3 * (p[1].z - p[0].z) };
-	Point v3 = { 3 * (p[3].x - p[2].x), 3 * (p[3].y - p[2].y), 3 * (p[3].z - p[2].z) };
 
-	glBegin(GL_LINE_STRIP);
-	for (float t = 0.0; t <= 1.0; t += 0.01)
-	{
-		float x = (1 - 3 * t*t + 2 * t*t*t)*p[0].x + t*(1 - t)*(1 - t)*v0.x + (3 * t*t - 2 * t*t*t)*p[3].x - t*t*(1 - t)*v3.x;
-		float y = (1 - 3 * t*t + 2 * t*t*t)*p[0].y + t*(1 - t)*(1 - t)*v0.y + (3 * t*t - 2 * t*t*t)*p[3].y - t*t*(1 - t)*v3.y;
-		float z = (1 - 3 * t*t + 2 * t*t*t)*p[0].z + t*(1 - t)*(1 - t)*v0.z + (3 * t*t - 2 * t*t*t)*p[3].z - t*t*(1 - t)*v3.z;
-		glVertex3f(x, y, z);
-	}
-	glEnd();
+	Hermite_Spline();
+	draw_point();
 
-	glPointSize(5);
-	glBegin(GL_POINTS);
-	glColor3f(1,0,0);
-	glVertex3f(test_point.x, test_point.y, test_point.z);
-	glEnd();
-	//glColor3f(0, 1, 0);
-	//glVertex3f(p[1].x, p[1].y, p[1].z);
-	//glColor3f(0, 0, 1);
-	//glVertex3f(p[2].x, p[2].y, p[2].z);
-	//glColor3f(1, 1, 0);
-	//glVertex3f(p[3].x, p[3].y, p[3].z);
-	//glEnd();
-
-	glColor3f(1, 1, 0);
-	glPushMatrix();
-	{
-		glTranslatef(p[0].x, p[0].y, p[0].z);
-		glutSolidCube(5);
-	}
-	glPopMatrix();
+	for (int i = 0; i < 100; i++)
+		printf("save_spline_point[%d].x = %.f, save_spline_point[%d].y = %.f, save_spline_point[%d].z = %.f \n", i, save_spline_point[i].x, i, save_spline_point[i].y, i, save_spline_point[i].z);
 
 	glPushMatrix();
-	{
-		glTranslatef(p[1].x, p[1].y, p[1].z);
-		glutSolidCube(5);
-	}
+	glTranslatef(save_spline_point[s_index].x, save_spline_point[s_index].y, save_spline_point[s_index].z);
+	glutSolidCube(20);
 	glPopMatrix();
-
-	glPushMatrix();
-	{
-		glTranslatef(p[2].x, p[2].y, p[2].z);
-		glutSolidCube(5);
-	}
-	glPopMatrix();
-
-	glPushMatrix();
-	{
-		glTranslatef(p[3].x, p[3].y, p[3].z);
-		glutSolidCube(5);
-	}
-	glPopMatrix();
-
-	printf("p[0].collision = %d \n", p[0].collison);
-	printf("p[1].collision = %d \n", p[1].collison);
-	printf("p[2].collision = %d \n", p[2].collison);
-	printf("p[3].collision = %d \n\n", p[3].collison);
-
 	glutSwapBuffers();
 }
 
@@ -163,6 +120,8 @@ void Mouse_Move(int x, int y)
 		if (p[i].collison)
 		{
 			p[i].z = 300 - y;
+			test_point.x = x - 400;
+			test_point.z = 300 - y;
 		}
 	}
 }
@@ -185,6 +144,12 @@ void keyboard(unsigned char key, int x, int y)
 
 void Timerfunction(int value)
 {
+	angle += 1;
+	if (camera_viewpoint == PERSPECTIVE){
+		if (s_index > 100)
+			s_index = 0;
+		s_index++;
+	}
 	glutPostRedisplay();
 	glutTimerFunc(40, Timerfunction, 1);
 }
@@ -207,9 +172,10 @@ void Cmera_change()
 	}
 	if (camera_viewpoint == PERSPECTIVE){
 		gluPerspective(60.0f, w1 / h1, 1.0, 2000.0);
-		gluLookAt(0.0, 1000, 0.0,
+		gluLookAt(0.0, 100, 1000.0,
 			0.0, 0.0, 0.0,
-			0.0, 0.0, -1.0);
+			0.0, 1.0, 0.0);
+		glRotatef(angle, 0, 1, 0);
 	}
 }
 
@@ -227,4 +193,65 @@ bool Collide_XZ(float a_x, float a_z, float b_x, float b_z, float a_radious, flo
 	if (top_a < bottom_b)
 		return false;
 	return true;
+}
+
+void draw_point()
+{
+	glColor3f(1, 1, 0);//ctrlpoint
+	glPushMatrix();
+	{
+		glTranslatef(p[0].x, p[0].y, p[0].z);
+		glutSolidCube(5);
+	}
+	glPopMatrix();
+
+	glPushMatrix();
+	{
+		glTranslatef(p[1].x, p[1].y, p[1].z);
+		glutSolidCube(5);
+	}
+	glPopMatrix();
+
+	glPushMatrix();
+	{
+		glTranslatef(p[2].x, p[2].y, p[2].z);
+		glutSolidCube(5);
+	}
+	glPopMatrix();
+
+	glPushMatrix();
+	{
+		glTranslatef(p[3].x, p[3].y, p[3].z);
+		glutSolidCube(5);
+	}
+	glPopMatrix();
+
+	glPointSize(5);//current mouse point
+	glBegin(GL_POINTS);
+	glColor3f(1, 0, 0);
+	glVertex3f(test_point.x, test_point.y, test_point.z);
+	glEnd();
+}
+void Hermite_Spline()
+{
+	glColor3f(1, 1, 1);
+	Point v0 = { 3 * (p[1].x - p[0].x), 3 * (p[1].y - p[0].y), 3 * (p[1].z - p[0].z) };
+	Point v3 = { 3 * (p[3].x - p[2].x), 3 * (p[3].y - p[2].y), 3 * (p[3].z - p[2].z) };
+
+	glBegin(GL_LINE_STRIP);
+	for (float t = 0.0; t <= 1.0; t += 0.01)
+	{
+		float x = (1 - 3 * t*t + 2 * t*t*t)*p[0].x + t*(1 - t)*(1 - t)*v0.x + (3 * t*t - 2 * t*t*t)*p[3].x - t*t*(1 - t)*v3.x;
+		float y = (1 - 3 * t*t + 2 * t*t*t)*p[0].y + t*(1 - t)*(1 - t)*v0.y + (3 * t*t - 2 * t*t*t)*p[3].y - t*t*(1 - t)*v3.y;
+		float z = (1 - 3 * t*t + 2 * t*t*t)*p[0].z + t*(1 - t)*(1 - t)*v0.z + (3 * t*t - 2 * t*t*t)*p[3].z - t*t*(1 - t)*v3.z;
+		glVertex3f(x, y, z);
+		int index = (int)(t * 100);
+		save_spline_point[index].x = x;
+		save_spline_point[index].y = y;
+		save_spline_point[index].z = z;
+	
+	}
+	glEnd();
+
+
 }
